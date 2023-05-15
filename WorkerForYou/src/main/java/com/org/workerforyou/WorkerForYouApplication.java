b50b6710f9f2ef0a533cda77dfcbe04697a90937
Rahul Dan <rahuldan98@gmail.com>
2023-05-16 00:12:49 +0530

    Structure completed

---
+++ WorkerForYou/src/main/java/com/org/workerforyou/WorkerForYouApplication.java
@@ -0,0 +1,13 @@
+package com.org.workerforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerForYouApplication.class, args);
+	}
+
+}
